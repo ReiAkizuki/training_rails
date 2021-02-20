d45b0ce753c5fd2865847a682f32c8d8fa355707
秋月嶺 <akidzukirei@akidzukireinoMacBook-Air.local>
2021-02-21 00:19:15 +0900

    ページ作成
    リダイレクト時に`You are being redirected.`と出るバグあり。

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,66 @@
+class BlogsController < ApplicationController
+  def index
+    @blogs = current_user.blogs.all
+  end
+
+  def show
+    id = params[:id]
+    begin
+      @blog = current_user.blogs.find(id)
+    rescue
+      render 'errors/404', status: :not_found and return
+    end
+  end
+
+  def new
+    @blog = current_user.blogs.new()
+  end
+
+  def create
+    @blog = current_user.blogs.new(blog_params)
+    if @blog.save
+      redirect_to :root, status: :ok, notice: 'Blog was successfully created.' and return
+    else
+      render :new, errors: @blog.errors and return
+    end
+  end
+
+  def edit
+    id = params[:id]
+    begin
+      @blog = current_user.blogs.find(id)
+    rescue
+      render 'errors/404', status: :not_found and return
+    end
+  end
+
+  def update
+    id = params[:id]
+    begin
+      @blog = current_user.blogs.find(id)
+    rescue
+      render 'errors/404', status: :not_found
+    end
+    if @blog.update(blog_params)
+      redirect_to @blog, status: :ok, notice: 'Blog was successfully updated.' and return
+    else
+      render :edit, errors: @blog.errors and return
+    end
+  end
+
+  def destroy
+    id = params[:id]
+    begin
+      @blog = current_user.blogs.find(id)
+    rescue
+      render 'errors  /404', status: :not_found
+    end
+    @blog.destroy
+  end
+
+  private
+
+  def blog_params
+    params.require(:blog).permit(:title, :text)
+  end
+end
---
+++ db/migrate/20210220124036_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :blogs do |t|
+      t.string     :title, null: false, default: ""
+      t.string     :text,  null: false, default: ""
+      t.references :user,  foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
